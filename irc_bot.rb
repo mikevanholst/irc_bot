dab1e0caf2ca05782c51ce151a410e64b2ec1351
Michael van Holst <mikevanholst@gmail.com>
2013-08-23 12:45:08 -0400

    initial commit of working irc bot Archie Brindleton

---
+++ irc_bot.rb
@@ -0,0 +1,36 @@
+require "socket"
+
+server = "chat.freenode.net"
+port = "6667"
+nick = "Mikes_Big_Bot"
+channel = "#bitmaker"
+greeting_prefix = "privmsg #bitmaker :"
+greetings = ["hi mike bot"]
+
+
+irc_server = TCPSocket.open(server, port)
+
+irc_server.puts "USER bhellobot 0 * BHelloBot"
+irc_server.puts "NICK #{nick}"
+irc_server.puts "JOIN #{channel}"
+#irc_server.puts "PRIVMSG #{channel} :Hello from IRB Bot"
+
+# Hello, Bot!
+until irc_server.eof? do
+  msg = irc_server.gets.downcase
+  puts msg
+
+  wasGreeted = false
+  greetings.each do |g|
+	wasGreeted = true if msg.include? g
+  end
+
+  if msg.include? greeting_prefix and wasGreeted
+  	response = "Oh hi Mike.  You are looking good today."
+  	irc_server.puts "PRIVMSG #{channel} :#{response}"
+  end
+
+
+
+end
+
